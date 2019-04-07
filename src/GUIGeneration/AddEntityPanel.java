d91dd6635c29c4490def72c9dc589d7e6a6156fa
Yashraj Kakkad <yashrajkakkad@gmail.com>
2019-04-07 10:41:27 +0530

    AddEntityPanel now extends CRUDPanel

--- src/GUIGeneration/AddEntityPanel.java
+++ src/GUIGeneration/AddEntityPanel.java
@@ -1,35 +1,16 @@
 package GUIGeneration;
 
-import Picocog.*;
 import CodeGeneration.*;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
-public class AddEntityPanel {
-
-    private final Entity entity;
-    private final PicoWriter w;
+public class AddEntityPanel extends CRUDPanel {
 
     public AddEntityPanel(Entity entity) throws IOException {
-        this.entity = entity;
-        w = new PicoWriter();
-        initialize();
-        generateLabels();
-        generateTextFields();
+        super(entity);
+        super.initialize("Add" + entity.getEntityName() + "Panel");
+        generateComponents();
         generateConstructor();
-        finish();
-        generateCode();
-    }
-
-    private void initialize() {
-        w.writeln("import javax.swing.*;");
-        w.writeln("import java.awt.*;");
-        w.writeln("import java.awt.Event.*;");
-        w.writeln("");
-        w.writeln_r("public class Add" + entity.getEntityName()
-                + "Panel extends JPanel {");
-        w.writeln("");
+        super.finish("Add" + entity.getEntityName() + "Panel");
     }
 
     private void generateLabels() {
@@ -46,30 +27,70 @@ public class AddEntityPanel {
         });
     }
 
-    private void generateAddComponents() {
+    private void generateButton() {
+        w.writeln("JButton addButton = new JButton(\"Add "
+                + entity.getEntityName() + "\");");
+    }
+
+    @Override
+    protected void generateComponents() {
+        generateLabels();
+        generateTextFields();
+        generateButton();
+    }
+
+    @Override
+    protected void generateAddComponents() {
         entity.getEntityMembers().forEach((entityMember) -> {
-            w.writeln("add(" + entityMember.getValue() + "Label);");
-            w.writeln("add(" + entityMember.getValue() + "TextField);");
-        });        
+            w.writeln("topPanel.add(" + entityMember.getValue() + "Label);");
+            w.writeln("topPanel.add(" + entityMember.getValue() + "TextField);");
+        });
+        w.writeln("bottomPanel.add(addButton);");
+        w.writeln("addButton.setMaximumSize(addButton.getPreferredSize());");
+    }
+
+    private void generateButtonActionListener() {
+        w.writeln_r("addButton.setOnAction((e) -> {");
+        StringBuilder insertQuery = new StringBuilder("INSERT INTO tbl_");
+        insertQuery.append(entity.getEntityName().toLowerCase())
+                .append(" VALUES (");
+        entity.getEntityMembers().forEach((entityMember) -> {
+            if(entityMember.getKey().equals("String") 
+                    || entityMember.getKey().equals("char"))
+                insertQuery.append('\'').append(entityMember.getValue())
+                        .append('\'').append(", ");
+            else {
+                insertQuery.append(entityMember.getValue()).append(", ");                
+            }
+        });
+        
+        insertQuery.deleteCharAt(insertQuery.length()-2);
+        insertQuery.append(");");
+        w.writeln("String insertQuery = \"" + insertQuery.toString() + "\";");
+        w.writeln("int i = DatabaseUtil.stmt.executeUpdate(insertQuery);");
+        w.writeln_r("if(i==1) {");
+        w.writeln("JOptionPane.showMessageDialog(this,\"" 
+                + entity.getEntityName() + " added successfully!\");");
+        w.writeln_lr("} else if {");
+        w.writeln("JOptionPane.showMessageDialog(this,"
+                + "\"Unexpected error occured. It might be due to "
+                + "faulty internet or duplication in your primary key!\");");
+        w.writeln_l("}");
+        w.writeln_l("});");        
     }
     
-    private void generateConstructor() {
+    @Override
+    protected void generateConstructor() {
         w.writeln_r("public Add" + entity.getEntityName() + "Panel() {");
-        w.writeln("setLayout(new GridLayout(" + entity.getEntityMembers().size()
+        w.writeln("setLayout(new BorderLayout());");
+        w.writeln("JPanel topPanel = new JPanel();");
+        w.writeln("JPanel bottomPanel = new JPanel();");
+        w.writeln("topPanel.setLayout(new GridLayout(" + entity.getEntityMembers().size()
                 + "," + entity.getEntityMembers().size() + ",10,10));");
         generateAddComponents();
+        generateButtonActionListener();
+        w.writeln("add(topPanel,BorderLayout.CENTER);");
+        w.writeln("add(bottomPanel, BorderLayout.SOUTH);");
         w.writeln_l("}");
-    }
-
-    private void finish() {
-        w.writeln_l("}");
-    }
-
-    private void generateCode() throws IOException {
-        File writeFile = new File(EntityManager.getDirectoryName()
-                + "\\Add" + entity.getEntityName() + "Panel.java");
-        FileWriter out = new FileWriter(writeFile);
-        out.write(w.toString());
-        out.close();
     }
 }
